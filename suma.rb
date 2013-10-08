8ef5ee7c411cb4f9483d16e1704051bab50fddf3
Javier Rodriguez <alu3992@cc399.(none)>
2013-10-08 11:08:13 +0100

    subiendo matriz suma

---
+++ suma.rb
@@ -0,0 +1,13 @@
+# matriz suma
+a = [[4, 3, 2], [7, 0, 5], [1, 1, 1]]
+b = [[1, 3, 8], [6, 9, 9], [2, 2, 5]]
+c = Array.new
+
+filaS, columnaS = a.size, a[1].size
+
+for i in 0...filaS do
+	c[i] = Array.new
+	for j in 0...columnaS do
+		c[i][j] = 0
+	end
+end
