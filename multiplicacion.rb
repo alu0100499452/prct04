b360b775f44e2257531d723866f1928377907b8f
Juan Lucas <alu0100499452@ull.edu.es>
2013-10-08 11:22:04 +0100

    Mutiplicacion hecha en rama MUL

--- multiplicacion.rb
+++ multiplicacion.rb
@@ -5,10 +5,24 @@ c = Array.new
 
 filaM, columnaM = d.size, d[1].size
 
-
 for i in 0...filaS do
 	c[i] = Array.new
 	for j in 0...columnaS do
 		c[i][j] = 0
 	end
 end
+
+# ==================== MULTIPLICACION ==========================
+
+	for i in 0...filaM do
+		for j in 0...columnaM do
+			for k in 0...filaM - 1 do
+				c[i][j] += d[i][k] * e[k][j]
+			end
+		end
+	end		
+	c.each do |filaM|
+		puts filaM.join(" ")
+	end
+
+# ==================== FIN MULTIPLICACION ========================
